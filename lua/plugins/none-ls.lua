4153cf6ea2a21b3c228ce80a5aceb975c24207c6
philipp <philipp248@proton.me>
2025-01-24 00:18:53 +0100

    added python lsp and formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,6 +8,8 @@ return {
 				null_ls.builtins.formatting.clang_format.with({
           extra_args = { "-style=LLVM" }
         }),
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
 			},
 		})
 
