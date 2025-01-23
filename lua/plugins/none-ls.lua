97aade108ae506d69637e1e9fb7f79c715c3c7b7
philipp <philipp248@proton.me>
2025-01-24 00:15:50 +0100

    added c++ fomratter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,14 +1,16 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.clang_format,
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.clang_format.with({
+          extra_args = { "-style=LLVM" }
+        }),
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "none-ls format" })
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "none-ls format" })
+	end,
 }
