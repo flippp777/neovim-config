fa8f1d9f22154cc58e507c32e6213695d7d0720f
philipp <philipp248@proton.me>
2025-01-23 20:59:54 +0100

    initial commit

---
+++ init.lua
@@ -0,0 +1,21 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+require("config.lazy")
+
+vim.cmd.colorscheme "catppuccin-mocha"
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "c", "cpp", "python", "rust"},
+  highlight = { enable = true },
+  indent = {enable = true},
+})
