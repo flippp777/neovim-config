0bc407adae2fea6c957b5ba543517fb040e4eebc
philipp <philipp248@proton.me>
2025-01-24 14:18:03 +0100

    harpoon config

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,9 +12,9 @@ return {
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
 		end, { desc = "Harpoon add" })
-		-- vim.keymap.set("n", "<C-e>", function()
-		--  harpoon.ui:toggle_quick_menu(harpoon:list())
-		--end)
+		vim.keymap.set("n", "<C-e>", function()
+		  harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, {desc = "Harpoon open quick menu"})
 
 		vim.keymap.set("n", "<C-h>", function()
 			harpoon:list():select(1)
@@ -36,28 +36,5 @@ return {
 		vim.keymap.set("n", "<C-S-N>", function()
 			harpoon:list():next()
 		end, { desc = "Harpoon next" })
-
-		local conf = require("telescope.config").values
-		local function toggle_telescope(harpoon_files)
-			local file_paths = {}
-			for _, item in ipairs(harpoon_files.items) do
-				table.insert(file_paths, item.value)
-			end
-
-			require("telescope.pickers")
-				.new({}, {
-					prompt_title = "Harpoon",
-					finder = require("telescope.finders").new_table({
-						results = file_paths,
-					}),
-					previewer = conf.file_previewer({}),
-					sorter = conf.generic_sorter({}),
-				})
-				:find()
-		end
-
-		vim.keymap.set("n", "<C-e>", function()
-			toggle_telescope(harpoon:list())
-		end, { desc = "Open harpoon window" })
 	end,
 }
