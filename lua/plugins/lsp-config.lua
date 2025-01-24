dc9d6965c8d87bbbcee17c83c02299779e267354
philipp <philipp248@proton.me>
2025-01-24 01:32:11 +0100

    added auto completion and snippets

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,26 +3,28 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd" ,"pyright"}
+        ensure_installed = { "lua_ls", "clangd", "pyright" },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'LSP hover information' })
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'LSP got to definition' })
-      vim.keymap.set({ 'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, { desc = 'LSP code action' })
-    end
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.clangd.setup({ capabilities = capabilities })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP hover information" })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP got to definition" })
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
+    end,
   },
 }
