e9023e805db558b463a5e463538bec97637c5400
philipp <philipp248@proton.me>
2025-01-31 02:30:30 +0100

    indent-blankline.nvim package

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,16 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	---@module "ibl"
+	---@type ibl.config
+	opts = {},
+	config = function()
+    local highlight = { "RainbowViolet"}
+    local hooks = require("ibl.hooks")
+
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function ()
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    end)
+		require("ibl").setup({ indent = { highlight = highlight } })
+	end,
+}
