c02dd23af328432d65a5e5115e55895ecdf2e44b
philipp <philipp248@proton.me>
2025-02-06 20:58:53 +0100

    new c++ style guide format

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.clang_format.with({
-          extra_args = { "-style=LLVM" }
+          extra_args = { "-style=GNU" }
         }),
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
