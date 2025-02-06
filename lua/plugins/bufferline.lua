b33e2a9ab124972b294f5fca637846c6f2b42ddb
philipp <philipp248@proton.me>
2025-02-06 17:22:21 +0100

    added bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,24 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		vim.opt.termguicolors = true
+		require("bufferline").setup({
+			options = {
+				mode = "buffers",
+				diagnostics = "nvim_lsp",
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+						highlight = "Directory",
+						separator = true,
+					},
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>bl", ":BufferLinePick<CR>")
+	end,
+}
