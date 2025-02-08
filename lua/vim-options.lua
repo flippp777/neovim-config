c5eb5f1bf8a748359875d3f5d770dcc20f47f269
philipp <philipp248@proton.me>
2025-02-08 14:47:06 +0100

    new movement keymaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,33 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.cmd("set relativenumber")
+
+
+-- Window keybinds
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-r>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<CR>")
+vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<CR>")
+vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<CR>")
+vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<CR>")
+
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
+
+vim.keymap.set("t", "<C-Up>", "<cmd>resize -2<CR>")
+vim.keymap.set("t", "<C-Down>", "<cmd>resize +2<CR>")
+vim.keymap.set("t", "<C-Left>", "<cmd>vertical resize -2<CR>")
+vim.keymap.set("t", "<C-Right>", "<cmd>vertical resize +2<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+
