c6a250dd91834a9073af9db7ce689f0a3681e37b
philipp <philipp248@proton.me>
2025-02-09 00:40:25 +0100

    changed harpoon back

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,36 +12,36 @@ return {
 		-- Telescope UI
 
 		-- basic telescope configuration
-		local conf = require("telescope.config").values
-		local function toggle_telescope(harpoon_files)
-			local file_paths = {}
-			for _, item in ipairs(harpoon_files.items) do
-				table.insert(file_paths, item.value)
-			end
-
-			require("telescope.pickers")
-				.new({}, {
-					prompt_title = "Harpoon",
-					finder = require("telescope.finders").new_table({
-						results = file_paths,
-					}),
-					previewer = conf.file_previewer({}),
-					sorter = conf.generic_sorter({}),
-				})
-				:find()
-		end
-
-		vim.keymap.set("n", "<leader>h", function()
-			toggle_telescope(harpoon:list())
-		end, { desc = "Harpoon" })
+		-- local conf = require("telescope.config").values
+		-- local function toggle_telescope(harpoon_files)
+		-- 	local file_paths = {}
+		-- 	for _, item in ipairs(harpoon_files.items) do
+		-- 		table.insert(file_paths, item.value)
+		-- 	end
+		--
+		-- 	require("telescope.pickers")
+		-- 		.new({}, {
+		-- 			prompt_title = "Harpoon",
+		-- 			finder = require("telescope.finders").new_table({
+		-- 				results = file_paths,
+		-- 			}),
+		-- 			previewer = conf.file_previewer({}),
+		-- 			sorter = conf.generic_sorter({}),
+		-- 		})
+		-- 		:find()
+		-- end
+		--
+		-- vim.keymap.set("n", "<leader>h", function()
+		-- 	toggle_telescope(harpoon:list())
+		-- end, { desc = "Harpoon" })
 
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
 		end, { desc = "Harpoon add" })
 
-		-- vim.keymap.set("n", "<leader>h", function()
-		-- 	harpoon.ui:toggle_quick_menu(harpoon:list())
-		-- end, { desc = "Harpoon open quick menu" })
+		vim.keymap.set("n", "<leader>h", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Harpoon" })
 
 		vim.keymap.set("n", "<C-1>", function()
 			harpoon:list():select(1)
