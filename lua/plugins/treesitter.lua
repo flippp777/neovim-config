647a81c106612b83c6f1fc4683e2244f82f4e1e1
flippp777 <96106374+flippp777@users.noreply.github.com>
2026-02-12 11:26:30 +0100

    Refactor Treesitter plugin configuration and build

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = function()
-      require("nvim-treesitter.install").update({ with_sync = true })()
-  end,
+    lazy = false,
+  build = ':TSUpdate',
   config = function()
-    local treesitter_path = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter"
-    if vim.fn.isdirectory(treesitter_path) == 1 then
-      vim.opt.rtp:prepend(treesitter_path)
-    end
-    local config = require("nvim-treesitter.configs")
+    local config = require("nvim-treesitter")
     config.setup({
       ensure_installed = {"lua", "c", "cpp", "python", "rust"},
       auto_install = true,
